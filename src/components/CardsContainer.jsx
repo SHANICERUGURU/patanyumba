79c8734433980f4ee520d60aecd8b419c9889737
SHANICERUGURU <rugurushanice990@gmail.com>
2024-06-26 12:51:27 +0300

    push changes

--- src/components/CardsContainer.jsx
+++ src/components/CardsContainer.jsx
@@ -7,65 +7,58 @@ import houseimg4 from "../assets/house4.jpg"
 import houseimg5 from "../assets/house5.jpg"
 import houseimg6 from "../assets/house6.jpg"
 const CardsContainer = () => {
-  let house1 = {
+  let houses=[
+  {
     housetitle: "penthouse",
     price: "250000",
     location: "Kileleshwa",
     img: houseimg1
-  }
-  let house2 = {
+  },
+  {
     housetitle: "cottage",
     price: "140000",
     location: "Utawala",
     img: houseimg2
-  }
-  let house3 = {
+  },
+ {
     housetitle: "bungalow",
     price: "140000",
     location: "Juja",
     img: houseimg3
-  }
-  let house4 = {
+  },
+{
     housetitle: "mansion",
     price: "250000",
     location: "Karen",
     img: houseimg4
-  }
-  let house5 = {
+  },
+ {
     housetitle: "mansion",
     price: "300000",
     location: "Runda",
     img: houseimg5
-  }
-  let house6 = {
+  },
+   {
     housetitle: "mansion",
     price: "250000",
     location: "Karen",
     img: houseimg6
   }
+]
   return (
     <>
       <div className="container houseContainer text-center">
         <h1>Get Your Dream Home</h1>
         <div className="row align-items-start">
-          <div className="col-md-4">
-            <HouseCard house={house1} />
-          </div>
-          <div className="col-md-4">
-            <HouseCard house={house2} />
-          </div>
-          <div className="col-md-4">
-            <HouseCard house={house3} />
-          </div>
-          <div className="col-md-4">
-            <HouseCard house={house4} />
-          </div>
-          <div className="col-md-4">
-            <HouseCard house={house5} />
-          </div>
-          <div className="col-md-4">
-            <HouseCard house={house6} />
-          </div>
+         {
+          houses.map((house,index)=>{
+            return(
+              <div className="col-md-3"key={index}>
+                <HouseCard house={house}/>
+              </div>
+            )
+})
+         }
         </div>
       </div>
     </>
--- src/components/housecard.jsx
+++ src/components/housecard.jsx
@@ -15,19 +15,26 @@ const HouseCard = ({ house }) => {
   }
   return (
     <>
+    {
+      house?
       <div className="card" style={{ width: '18rem', marginTop: '15px' }}>
-        <img src={house.img} className="card-img-top" alt="..." />
+        <img src={ house ? house.img :"..."} className="card-img-top" alt="..." />
         <div className="card-body">
 
           <h5 className="card-title">{house.housetitle}</h5>
           <h4 className="card-title">{house.price}kshs</h4>
           <h4 className="card-title">location:{house.location}</h4>
+          <p>Category:{house.price>100000? "expensive":"cheap"}</p>
           <h4>likes:{likes}</h4>
           <button type="button" class="btn btn-light" onClick={changePrice}>get house</button>
           <button type="button" class="btn btn-warning" onClick={incLikes}>like!!</button>
         </div>
       </div>
+      :
+      <h1>No house</h1>
+    }
     </>
+
   )
 }
 export default HouseCard
